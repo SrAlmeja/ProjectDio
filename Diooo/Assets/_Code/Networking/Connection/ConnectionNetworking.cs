7eac769ee34697308748525ec8e278e6cd95d4f2
Dino <dino.@gmail.com>
2023-03-01 16:53:24 -0600

    has internet

--- Diooo/Assets/_Code/Networking/Connection/ConnectionNetworking.cs
+++ Diooo/Assets/_Code/Networking/Connection/ConnectionNetworking.cs
@@ -4,22 +4,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace com.LazyGames.Dio
-{ 
+{
     public class ConnectionNetworking : MonoBehaviour
     {
 
-        private void Awake()
+        private bool _hasInternet;
+
+        public bool HasInternet => _hasInternet;
+
+    private void Awake()
         {
         }
 
         void Start()
         {
-
+            InitializerNetworking.Instance.OnFinishLoading += Initialize;
         }
 
         void Update()
         {
 
         }
+
+        void Initialize()
+        {
+            CheckConnection();
+        }
+
+        void CheckConnection()
+        {
+            
+        }
+
     }
 }
