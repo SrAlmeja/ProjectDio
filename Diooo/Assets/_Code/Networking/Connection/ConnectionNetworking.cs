b46f4686e696acf01322484539f434cf3b528756
Dino <dino.@gmail.com>
2023-03-01 16:51:25 -0600

    upload Dino branch

---
+++ Diooo/Assets/_Code/Networking/Connection/ConnectionNetworking.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.LazyGames.Dio
+{ 
+    public class ConnectionNetworking : MonoBehaviour
+    {
+
+        private void Awake()
+        {
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Diooo/Assets/_Code/Networking/Initializer/InitializerNetworking.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.LazyGames.Dio
+{ 
+    public class InitializerNetworking : MonoBehaviour
+    {
+        public static InitializerNetworking Instance;
+        public event Action OnFinishLoading; 
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
