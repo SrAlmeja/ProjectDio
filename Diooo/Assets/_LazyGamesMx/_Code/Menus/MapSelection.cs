13f5e281cfc7c40e5a005504589ee5efbd0edeb1
AtraxxDev <cdmx2218@amerike.edu.mx>
2023-03-15 16:55:42 -0600

    MenuSelection

---
+++ Diooo/Assets/_LazyGamesMx/_Code/Menus/MapSelection.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSelection : MonoBehaviour
+{
+    [SerializeField] private GameObject _pivot;
+
+    private void Start()
+    {
+        ChangeSelection(true);
+    }
+
+    private void ChangeSelection(bool isForward)
+    {
+        if (isForward) iTween.RotateAdd(_pivot, new Vector3(0, 45f, 0f), 2f);
+        else iTween.RotateAdd(_pivot, new Vector3(0,-45f,0f), 2f);
+    }
+}
