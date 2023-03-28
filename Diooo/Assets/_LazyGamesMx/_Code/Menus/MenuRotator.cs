c38f961de1a55061f05a999a7ad8d7e2c26a2cf0
AtraxxDev <cdmx2218@amerike.edu.mx>
2023-03-28 16:52:58 -0600

    MenuRotator

--- Diooo/Assets/_LazyGamesMx/_Code/Menus/MenuRotator.cs
+++ Diooo/Assets/_LazyGamesMx/_Code/Menus/MenuRotator.cs
@@ -6,19 +6,26 @@ namespace com.LazyGames.Dio
     {
         [Header("Channel SO Dependencies")]
         [SerializeField] private GameObjectFloatChannelSO RotationRequestChannel;
+        [SerializeField] private GameObjectFloatChannelSO MoveRequestChannel;
 
         private void OnEnable()
         {
-            RotationRequestChannel.GameObjectFloatEvent += RotateObject;
+            RotationRequestChannel.GameObjectFloatEvent += RotateMenu;
+            MoveRequestChannel.GameObjectFloatEvent += MoveMenu;
         }
         private void OnDisable()
         {
-            RotationRequestChannel.GameObjectFloatEvent -= RotateObject;
+            RotationRequestChannel.GameObjectFloatEvent -= RotateMenu;
         }
 
-        private void RotateObject(GameObject senderPivot, float position)
+        private void RotateMenu(GameObject senderPivot, float position)
         {
             iTween.RotateTo(senderPivot, new Vector3(0f,45f*position,0f), 3f);
+        }
+
+        private void MoveMenu(GameObject mainPivot, float position)
+        {
+            iTween.MoveTo(mainPivot, new Vector3(0f, 0f, 0f), 3f);
         }
     }
 }
