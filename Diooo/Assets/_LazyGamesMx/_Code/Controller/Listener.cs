bd7d4ab910db1465009aae45d4c2bf4ba0a45102
Leinad DH <daniel_navarrete_05@hotmail.com>
2023-03-28 16:06:54 -0600

    Funtional Controller by events

--- Diooo/Assets/_LazyGamesMx/_Code/Controller/Listener.cs
+++ Diooo/Assets/_LazyGamesMx/_Code/Controller/Listener.cs
@@ -1,24 +1,50 @@
-using com.LazyGames.Dio;
-using System.Collections;
-using System.Collections.Generic;
+//Daniel Navarrete 28/03/2023  Controller by events
+
 using UnityEngine;
 
-public class Listener : MonoBehaviour
+namespace com.LazyGames.Dio
 {
-    public FloatEventChannelSO HanbreakeEvent;
-
-    private void OnEnable()
+    public class Listener : MonoBehaviour
     {
-        HanbreakeEvent.FloatEvent += HandBrake;
-    }
+        public BoolEventChannelSO HanbreakeEvent;
+        public BoolEventChannelSO StopTimeEvent;
+        public FloatEventChannelSO AngleEvent;
+        public FloatEventChannelSO TorqueEvent;
 
-    private void OnDisable()
-    {
-        HanbreakeEvent.FloatEvent -= HandBrake;
-    }
+        private void OnEnable()
+        {
+            HanbreakeEvent.BoolEvent += HandBrake;
+            StopTimeEvent.BoolEvent += StopTime;
+            AngleEvent.FloatEvent += Angle;
+            TorqueEvent.FloatEvent += Torque;
+        }
 
-    void HandBrake(float f)
-    {
-        Debug.Log("Si funciona :D: " + f);
+        private void OnDisable()
+        {
+            HanbreakeEvent.BoolEvent -= HandBrake;
+            StopTimeEvent.BoolEvent -= StopTime;
+            AngleEvent.FloatEvent -= Angle;
+            TorqueEvent.FloatEvent -= Torque;
+        }
+
+        void HandBrake(bool b)
+        {
+            Debug.Log("Valor booleano: " + b);
+        }
+
+        void StopTime(bool b)
+        {
+            Debug.Log("Valor booleano: " + b);
+        }
+
+        void Angle(float f)
+        {
+            Debug.Log("Flotante Uno: " + f);
+        }
+
+        void Torque(float f)
+        {
+            Debug.Log("Flotante Dos: " + f);
+        }
     }
 }
