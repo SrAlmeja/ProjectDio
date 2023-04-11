9efbacbe1cefbfd57cf992bb6174343280d59bd8
AtraxxDev <cdmx2218@amerike.edu.mx>
2023-04-11 16:47:52 -0600

    Menu Navigator and int SO Change

--- Diooo/Assets/_LazyGamesMx/_Code/Menus/MenuNavigator.cs
+++ Diooo/Assets/_LazyGamesMx/_Code/Menus/MenuNavigator.cs
@@ -11,6 +11,9 @@ namespace com.LazyGames.Dio
 {
     public class MenuNavigator : MonoBehaviour
     {
+        [Header("SO Channel Dependencies")]
+        [SerializeField] private IntEventChannelSO _navigationRequestChannel;
+
         [Header("Initial Dependency")]
         [SerializeField] private MenuAdress _currentAdress;
 
@@ -19,9 +22,43 @@ namespace com.LazyGames.Dio
             _currentAdress = newAdress;
         }
 
-        private void CheckDeadEnd(MenuAdress adress)
+        private bool CheckDeadEndForward(MenuAdress adress)
         {
-           
+            return adress.ForwardTo == null;
+        }
+
+        private bool CheckDeadEndBackward(MenuAdress adress)
+        {
+            return adress.BackTo == null;
+        }
+
+        private void MoveForward()
+        {
+            if (CheckDeadEndForward(_currentAdress))
+            {
+                _currentAdress.DoForwardAction();
+                return;
+            }
+
+            RequestNavigation(_currentAdress.ForwardTo);
+        }
+
+        private void MoveBackward()
+        {
+            if (CheckDeadEndBackward(_currentAdress))
+            {
+                _currentAdress.DoBackAction();
+                return;
+            }
+
+            RequestNavigation(_currentAdress.BackTo);
+        }
+
+        private void RequestNavigation(MenuAdress toAdress)
+        {
+            //TODO once received the new adress we want to go.
+
+            _navigationRequestChannel.DoubleIntEvent(toAdress.Pivot, toAdress.Number);
         }
     }
 }
