33370e797d32854cdacb447b14d71d455b0269c3
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2023-04-14 18:01:28 -0600

    Ya se mueven en cada instancia pero spawnea 4 cabrones

--- Diooo/Assets/_LazyGamesMx/_Code/Managers/DioGameManager.cs
+++ Diooo/Assets/_LazyGamesMx/_Code/Managers/DioGameManager.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace com.LazyGames.Dio
 {
@@ -18,7 +19,8 @@ namespace com.LazyGames.Dio
 
         #region private variables
 
-        [SerializeField] private GameObject _playerCarPrefab;
+        [SerializeField] private List<Transform> placesToSpawnCars;
+        [SerializeField] private Transform playerCarPrefab;
 
 
         #endregion
@@ -44,7 +46,7 @@ namespace com.LazyGames.Dio
             Instance = this;
             if (IsServer)
             {
-                Debug.Log("Server is loading the scene");
+                // Debug.Log("Server is loading the scene");
                 NetworkManager.Singleton.SceneManager.OnLoadEventCompleted += SceneManager_OnLoadEventCompleted;
             }
         }
@@ -57,11 +59,26 @@ namespace com.LazyGames.Dio
             UnityEngine.SceneManagement.LoadSceneMode loadSceneMode, List<ulong> clientsCompleted,
             List<ulong> clientsTimedOut)
         {
-            foreach (ulong clientID in NetworkManager.Singleton.ConnectedClientsIds)
+            
+            Debug.Log("<color=#C9FE3B>Number of players connected </color>" + NetworkManager.Singleton.ConnectedClientsIds.Count);
+            
+            int spawnIndex = 0;
+            foreach (var clientID in NetworkManager.Singleton.ConnectedClientsIds)
             {
-                GameObject playerTransform = Instantiate(_playerCarPrefab);
-                playerTransform.GetComponent<NetworkObject>().SpawnAsPlayerObject(clientID, true);
-                Debug.Log("Spawned player for client: " + clientID);
+                spawnIndex++;
+                if (spawnIndex >= placesToSpawnCars.Count)
+                {
+                    Debug.Log("<color=#FE4A3B>Not enough spawn points</color>");
+                    return;
+                }
+                
+                Debug.Log("<color=#C9FE3B>Spawned index players = </color>"+ spawnIndex);
+                
+                Transform playerTransform = Instantiate(playerCarPrefab);
+                playerTransform.position = placesToSpawnCars[spawnIndex].position;
+                NetworkObject networkObject = playerTransform.GetComponent<NetworkObject>();
+                networkObject.SpawnAsPlayerObject(clientID, true);
+                Debug.Log("<color=#7AEFFF>Spawned player for client: </color>" + clientID + gameObject.name); 
             }
         }
 
