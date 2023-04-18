60be7598c629d4f01f49e18911078d92f60001f7
Sr Almeja <cdmx1895@amerike.edu.mx>
2023-04-18 16:46:15 -0600

    Avances de hoy GridData

---
+++ Diooo/Assets/_LazyGamesMx/_Code/Almeja Code/BuildSystem/Grid Placement system 2.0i/DataGrid.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class DataGrid
+{
+    private Dictionary<Vector3Int, PlacementData> placedObject = new();
+
+    public void AddObjectAt(Vector3Int gridPosition, Vector2Int objectSize, int ID, int placedobjectIndex)
+    {
+        List<Vector3Int> positionToOccupy = CalculatePositions(gridPosition, objectSize);
+        PlacementData data = new PlacementData(positionToOccupy, ID, placedobjectIndex);
+        foreach (var pos in positionToOccupy)
+        {
+            if (placedObject.ContainsKey(pos))
+                throw new Exception($"Dictionary already Contains this cell position {pos}");
+                placedObject[pos] = data;
+        }
+    }
+
+    private List<Vector3Int> CalculatePositions(Vector3Int gridPosition, Vector2Int objectSize)
+    {
+        List<Vector3Int> returnVal = new();
+        for (int x = 0; x < objectSize.x; x++)
+        {
+            for (int y = 0; y < objectSize.y; y++)
+            {
+                returnVal.Add(gridPosition + new Vector3Int(x,y,0));
+            }
+        }
+        return returnVal;
+    }
+
+    public bool CanPlaceObjectAt(Vector3Int gridPosition, Vector2Int objectSize)
+    {
+        List<Vector3Int> positionOccupy = CalculatePositions(gridPosition, objectSize);
+        foreach (var pos in positionOccupy)
+        {
+            if (placedObject.ContainsKey(pos))
+                return false;
+        }
+
+        return true;
+    }
+}
+
+
+    public class PlacementData
+{
+    public List<Vector3Int> occupiedPositions;
+    public int ID { get; private set; }
+    public int PlacedObjectIndex { get; private set; }
+
+    public PlacementData(List<Vector3Int> occupiedPositions, int iD, int placedObjectIndex)
+    {
+        ID = iD;
+        placedObjectIndex = placedObjectIndex;
+    }
+}
+
