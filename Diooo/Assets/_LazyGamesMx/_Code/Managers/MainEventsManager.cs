8ede5f61787cf1723ac32d9acfab8da720f06e11
AtraxxDev <cdmx2218@amerike.edu.mx>
2023-05-02 16:04:22 -0600

    MainEventManager WIP

---
+++ Diooo/Assets/_LazyGamesMx/_Code/Managers/MainEventsManager.cs
@@ -0,0 +1,14 @@
+//Fernando Cossio 02/05/2023
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.LazyGames.Dio
+{
+    public class MainEventsManager : MonoBehaviour
+    {
+        
+       
+    }
+}
