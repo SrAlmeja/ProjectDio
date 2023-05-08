af67d6a3908dcae01e678a39d9cab8985ee8ca73
CryoStorage <iracoon2014@Gmail.com>
2023-05-08 13:57:38 -0600

    NetworkCamera updated. untested

--- Diooo/Assets/GameDevChef/NetworkCameraFollow.cs
+++ Diooo/Assets/GameDevChef/NetworkCameraFollow.cs
@@ -1,46 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
+// Creado Raymundo Mosqueda 24/04/2023
+//
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class NetworkCameraFollow : NetworkBehaviour
+namespace com.LazyGames.Dio
 {
-    
-    [SerializeField] private Vector3 offset = new Vector3(0,4,-10);
-    [SerializeField] private Transform target;
-    [SerializeField] private float translateSpeed = 10;
-    [SerializeField] private float rotationSpeed = 10;
+    public class NetworkCameraFollow : NetworkBehaviour
+    {
+        // [Header("Configurable Values")]
+        [SerializeField] private Vector3 offset = new Vector3(0,4,-10);
+        [SerializeField] private float translateSpeed = 10;
+        [SerializeField] private float rotationSpeed = 10;
 
-    [HideInInspector]public bool HasTarget;
+        [HideInInspector]public bool hasTarget;
+        private Transform _target;
 
-    public void SetTarget(GameObject target)
-    {
-        this.target = target.transform;
-    }
+        public void SetTarget(GameObject target)
+        {
+            if(!IsOwner) return;
+            this._target = target.transform;
+            hasTarget = true;
+        }
 
-    IEnumerator WaitForTarget()
-    {
-        yield return new WaitUntil(() =>HasTarget); 
-    }
-    
-    private void FixedUpdate()
-    {
-        if(!IsOwner) return;
-        HandleTranslation();
-        HandleRotation();
-    }
-   
-    private void HandleTranslation()
-    {
-        if(!IsOwner) return;
-        var targetPosition = target.TransformPoint(offset);
-        transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
-    }
-    private void HandleRotation()
-    {
-        if(!IsOwner) return;
-        var direction = target.position - transform.position;
-        var rotation = Quaternion.LookRotation(direction, Vector3.up);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        private void FixedUpdate()
+        {
+            if (!hasTarget) return;
+            if(!IsOwner) return;
+            HandleTranslation();
+            HandleRotation();
+        }
+       
+        private void HandleTranslation()
+        {
+            if(!IsOwner) return;
+            var targetPosition = _target.TransformPoint(offset);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
+        }
+        private void HandleRotation()
+        {
+            if(!IsOwner) return;
+            var direction = _target.position - transform.position;
+            var rotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        }
     }
 }
