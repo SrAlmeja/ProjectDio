0bf3dac23c18a2179f9ff3f5a99812b392825f72
Dino <dino.@gmail.com>
2023-05-18 16:24:33 -0600

    Menu add new scenes

--- Diooo/Assets/_LazyGamesMx/_Code/Managers/SceneKeys.cs
+++ Diooo/Assets/_LazyGamesMx/_Code/Managers/SceneKeys.cs
@@ -5,13 +5,13 @@
         public static string MAIN_MENU_SCENE = "MainMenu";
         public static string LOBBY_SCENE = "Lobby_Dio";
         public static string GAME_SCENE = "Race_Multiplayer";
-        public static string GAME_SCENE_SINGLEPLAYER = "Race_Multiplayer";
+        public static string GAME_SCENE_SINGLEPLAYER = "Race_Singleplayer";
         
-        public static string GAME_TUTORIAL = "Tutorial";
-        public static string GAME_LEVEL_1 = "Level_1";
-        public static string GAME_LEVEL_2 = "Level_2";
-        public static string GAME_LEVEL_3 = "Level_3";
-        public static string GAME_LEVEL_4 = "Level_4";
+        public static string GAME_TUTORIAL = "Dio_Level_Tutorial";
+        public static string GAME_LEVEL_1 = "Dio_Level_01";
+        public static string GAME_LEVEL_2 = "Dio_Level_02";
+        public static string GAME_LEVEL_3 = "Dio_Level_03";
+        public static string GAME_LEVEL_4 = "Dio_Level_04";
         
     }
 }
