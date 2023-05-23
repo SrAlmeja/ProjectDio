dedb1ff56cd4d62c986a3961488d43e38b9c2309
LeinadDH <daniel_navarrete_05@hotmail.com>
2023-05-23 16:43:58 -0600

    Changes Goal

--- Diooo/Assets/_LazyGamesMx/_Code/Goal/Goal.cs
+++ Diooo/Assets/_LazyGamesMx/_Code/Goal/Goal.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace com.LazyGames.Dio
@@ -7,9 +9,49 @@ namespace com.LazyGames.Dio
         [SerializeField] private VoidEventChannelSO _actualLap;
         [SerializeField] private BoolEventChannelSO _isReverse;
         private bool _reverse = false;
+        private Dictionary<GameObject, bool> autosDictionary;
+
+        void Start()
+        {
+            autosDictionary = new Dictionary<GameObject, bool>();
+
+            GameObject[] autos = GameObject.FindGameObjectsWithTag("Player");
+
+            for (int i = 0; i < autos.Length; i++)
+            {
+                GameObject auto = autos[i];
+                bool estadoInicial = true; 
+
+                autosDictionary.Add(auto, estadoInicial);
+            }
+
+            foreach (KeyValuePair<GameObject, bool> pair in autosDictionary)
+            {
+                GameObject auto = pair.Key;
+                bool estado = pair.Value;
+
+                int numeroAuto = auto.GetInstanceID();
+                Debug.Log("Auto: " + numeroAuto + ", Estado: " + estado);
+            }
+        }
 
         private void OnTriggerEnter(Collider other)
         {
+            GameObject objetoColisionado = other.gameObject;
+
+            // Verificar si el objeto está en el diccionario
+            if (autosDictionary.ContainsKey(objetoColisionado))
+            {
+                // Cambiar el valor booleano del objeto
+                autosDictionary[objetoColisionado] = false;
+
+                // Realizar otras acciones o lógica aquí según sea necesario
+
+                // Ejemplo: Imprimir el estado actual del objeto
+                bool estadoObjeto = autosDictionary[objetoColisionado];
+                Debug.Log("Estado del objeto: " + estadoObjeto);
+            }
+
             if (other.CompareTag("Player"))
             {
                 BoxCollider boxCollider = GetComponent<BoxCollider>();
