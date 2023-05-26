60b593e1e51c226ef3b3c3445b70eb6dbe63894b
LeinadDH <daniel_navarrete_05@hotmail.com>
2023-05-25 18:02:02 -0600

    Multiplayer Goal Working

--- Diooo/Assets/_LazyGamesMx/_Code/Goal/Goal.cs
+++ Diooo/Assets/_LazyGamesMx/_Code/Goal/Goal.cs
@@ -6,85 +6,65 @@ namespace com.LazyGames.Dio
 {
     public class Goal : MonoBehaviour
     {
-        [SerializeField] private VoidEventChannelSO _actualLap;
-        [SerializeField] private BoolEventChannelSO _isReverse;
-        private bool _reverse = false;
-        private Dictionary<GameObject, bool> autosDictionary;
+        private Dictionary<AutoData, int> vueltasPorAuto = new Dictionary<AutoData, int>();
+        private Dictionary<AutoData, bool> direccionCorrectaPorAuto = new Dictionary<AutoData, bool>();
+        [SerializeField] private int vueltasNecesarias;
+        private int autosCompletados;
 
-        void Start()
+        private void Start()
         {
-            autosDictionary = new Dictionary<GameObject, bool>();
+            AutoData[] autos = FindObjectsOfType<AutoData>();
 
-            GameObject[] autos = GameObject.FindGameObjectsWithTag("Player");
-
-            for (int i = 0; i < autos.Length; i++)
+            foreach (AutoData auto in autos)
             {
-                GameObject auto = autos[i];
-                bool estadoInicial = true; 
-
-                autosDictionary.Add(auto, estadoInicial);
-            }
-
-            foreach (KeyValuePair<GameObject, bool> pair in autosDictionary)
-            {
-                GameObject auto = pair.Key;
-                bool estado = pair.Value;
-
-                int numeroAuto = auto.GetInstanceID();
-                Debug.Log("Auto: " + numeroAuto + ", Estado: " + estado);
+                vueltasPorAuto.Add(auto, 0);
+                direccionCorrectaPorAuto.Add(auto, true);
             }
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            GameObject objetoColisionado = other.gameObject;
+            AutoData auto = other.GetComponent<AutoData>();
 
-            // Verificar si el objeto está en el diccionario
-            if (autosDictionary.ContainsKey(objetoColisionado))
+            if (auto != null && direccionCorrectaPorAuto.ContainsKey(auto))
             {
-                // Cambiar el valor booleano del objeto
-                autosDictionary[objetoColisionado] = false;
+                Vector3 direccionDeColision = other.ClosestPoint(transform.position) - transform.position;
+                Vector3 direccionFrontal = transform.forward;
 
-                // Realizar otras acciones o lógica aquí según sea necesario
+                bool esCaraFrontal = Vector3.Dot(direccionDeColision.normalized, direccionFrontal) > 0.5f;
 
-                // Ejemplo: Imprimir el estado actual del objeto
-                bool estadoObjeto = autosDictionary[objetoColisionado];
-                Debug.Log("Estado del objeto: " + estadoObjeto);
-            }
-
-            if (other.CompareTag("Player"))
-            {
-                BoxCollider boxCollider = GetComponent<BoxCollider>();
-
-                Vector3 closestPoint = boxCollider.ClosestPoint(other.transform.position);
-
-                Vector3 direction = closestPoint - transform.position;
-
-                Vector3 frontDirection = transform.forward;
-
-                float dotProduct = Vector3.Dot(direction, frontDirection);
-
-                float threshold = 0.0f;
-
-                if (dotProduct >= threshold)
+                if (esCaraFrontal)
                 {
-                    if (_reverse) 
+                    if (direccionCorrectaPorAuto[auto])
                     {
-                        _isReverse.BoolEvent(false);
-                        _reverse = false;
-                        return;
+                        vueltasPorAuto[auto]++;
                     }
-                    else if(!_reverse)
+                    else
                     {
-                        _actualLap.VoidEvent();
+                        direccionCorrectaPorAuto[auto] = true;
                     }
                 }
                 else
                 {
-                    _isReverse.BoolEvent(true);
-                    _reverse = true;
+                    direccionCorrectaPorAuto[auto] = false;
+                }
+
+                if (vueltasPorAuto[auto] == vueltasNecesarias)
+                {
+                    Debug.Log("¡El auto " + auto.name + " ha completado todas las vueltas requeridas!");
+                    autosCompletados++;
+
+                    if (autosCompletados == vueltasPorAuto.Count)
+                    {
+                        OnAllAutosCompleted();
+                    }
                 }
             }
+        }
+
+        private void OnAllAutosCompleted()
+        {
+            Debug.Log("¡Todos los autos han completado las vueltas requeridas!");
         }
     }
 }
