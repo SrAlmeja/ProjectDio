2d2bfe0709464c9e3153808f18a30ae94aa3450d
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2023-06-03 23:25:36 -0600

    Avances goal multiplayer

--- Diooo/Assets/_LazyGamesMx/_Code/Goal/MultiplayerGoal.cs
+++ Diooo/Assets/_LazyGamesMx/_Code/Goal/MultiplayerGoal.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace com.LazyGames.Dio
 {
-    public class MultiplayerGoal : MonoBehaviour
+    public class MultiplayerGoal :  NetworkBehaviour
     {
         private Dictionary<AutoData, int> vueltasPorAuto = new Dictionary<AutoData, int>();
         private Dictionary<AutoData, bool> direccionCorrectaPorAuto = new Dictionary<AutoData, bool>();
         [SerializeField] private int vueltasNecesarias;
         private int autosCompletados;
+        
 
-        private void Start()
+        public override void OnNetworkSpawn()
+        {
+            DioGameManagerMultiplayer.Instance.OnGameStateChange += HandleGameStateChange;
+        }
+
+        private void HandleGameStateChange(DioGameManagerMultiplayer.GameStates state)
+        {
+            if (state == DioGameManagerMultiplayer.GameStates.GamePlaying)
+            {
+
+                FoundAutos();
+            }
+        }
+        
+        private void FoundAutos()
         {
             AutoData[] autos = FindObjectsOfType<AutoData>();
 
@@ -20,19 +36,24 @@ namespace com.LazyGames.Dio
                 vueltasPorAuto.Add(auto, 0);
                 direccionCorrectaPorAuto.Add(auto, true);
             }
+            
+            Debug.Log("<color=#DFFE97>Found Autos + </color>" + autos.Length);
+
         }
 
         private void OnTriggerEnter(Collider other)
         {
             AutoData auto = other.GetComponent<AutoData>();
-
+            Debug.Log("<color=#DFFE97>On trigger + = </color>" +  auto.name);
+            
+            
             if (auto != null && direccionCorrectaPorAuto.ContainsKey(auto))
             {
                 Vector3 direccionDeColision = other.ClosestPoint(transform.position) - transform.position;
                 Vector3 direccionFrontal = transform.forward;
-
+            
                 bool esCaraFrontal = Vector3.Dot(direccionDeColision.normalized, direccionFrontal) > 0.5f;
-
+            
                 if (esCaraFrontal)
                 {
                     if (direccionCorrectaPorAuto[auto])
@@ -48,15 +69,16 @@ namespace com.LazyGames.Dio
                 {
                     direccionCorrectaPorAuto[auto] = false;
                 }
-
+            
                 if (vueltasPorAuto[auto] == vueltasNecesarias)
                 {
                     Debug.Log("¡El auto " + auto.name + " ha completado todas las vueltas requeridas!");
                     autosCompletados++;
-
+            
                     if (autosCompletados == vueltasPorAuto.Count)
                     {
-                        OnAllAutosCompleted();
+                        if(IsServer)
+                            OnAllAutosCompleted();
                     }
                 }
             }
@@ -67,5 +89,6 @@ namespace com.LazyGames.Dio
             Debug.Log("¡Todos los autos han completado las vueltas requeridas!");
             DioGameManagerMultiplayer.Instance.OnPlayersCompleteRace?.Invoke();
         }
+        
     }
 }
