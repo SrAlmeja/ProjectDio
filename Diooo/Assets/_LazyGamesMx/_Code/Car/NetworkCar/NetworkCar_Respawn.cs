37c1e1f032446de0f74d04931168944b90695a35
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2023-06-06 21:09:40 -0600

    meta multiplayer y cambios del lobby

---
+++ Diooo/Assets/_LazyGamesMx/_Code/Car/NetworkCar/NetworkCar_Respawn.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using System.Collections.Generic;
+using com.LazyGames.Dio;
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkCar_Respawn : NetworkBehaviour
+{
+        [Header("Car Parameters Scriptable Object")]
+        [SerializeField] private CarParametersSo carParametersSo;
+        
+        [Header("Serialized References")]
+        [SerializeField] private GameObject healthIndicator;
+        [SerializeField]private GameObject visuals;
+        private Renderer _healthIndicatorRenderer;
+
+        
+        private float _health;
+        private float _maxHealth;
+        private float _respawnTime;
+        private float _elapsedTime;
+        private float _damageCooldown;
+        private float _minDamage;
+        private float _maxDamage;
+        private Vector3 _respawnPosition;
+        private Quaternion _respawnRotation;
+        private NetworkCarImpulse _carImpulse;
+        private Rigidbody _rb;
+        
+        [HideInInspector]public bool isDead;
+
+        private void Start()
+        {
+            Prepare();
+            _health = _maxHealth;
+        }
+
+        private void Update()
+        {
+            CheckHealth();
+            
+            _elapsedTime += Time.fixedDeltaTime;
+            _rb. isKinematic = isDead;
+            _healthIndicatorRenderer.material.SetFloat("_FillValue", Remap(_health));
+        }
+
+        private void CheckHealth()
+        {
+            if (_health > 0) return;
+            Explode();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("CheckPoint")) return;
+            Transform t = other.transform;
+            UpdateCheckPoint(t.position, t.rotation);
+        }
+
+        private void UpdateCheckPoint(Vector3 pos, Quaternion rot)
+        {
+            _respawnPosition = pos;
+            _respawnRotation = rot;
+            // Debug.Log($"updated checkpoint to {pos}");
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (_elapsedTime < _damageCooldown) return;
+            float mag  = other.relativeVelocity.magnitude;
+            TakeDamage(CalculateDamage(mag));
+            _elapsedTime = 0;
+        }
+        
+        private float CalculateDamage(float mag)
+        {
+            float dmg = mag * 0.5f;
+            switch (dmg)
+            {
+                case float f when (f < _minDamage):
+                    return 1;
+                
+                case float f when (f > _maxDamage):
+                    return _maxDamage;
+                
+                default: return dmg;
+            }
+        }
+
+        private void TakeDamage(float damage)
+        {
+            _health -= damage;
+            // Debug.Log($"took {damage} dmg");
+        }
+
+        float Remap(float value)
+        {
+            float normalizedValue = Mathf.InverseLerp(0, 100, value);
+            return Mathf.Lerp(0, 1, normalizedValue);
+        }
+        
+        private void Respawn()
+        {
+            isDead = false;
+            transform.position = _respawnPosition;
+            transform.rotation = _respawnRotation;
+            _health = _maxHealth;
+            visuals.SetActive(true);
+            _rb.velocity = Vector3.zero;
+            StopCoroutine(CorWaitToRespawn());
+        }
+
+        private void Punched()
+        {
+            TakeDamage(carParametersSo.PunchDamage);
+        }
+
+        private IEnumerator CorWaitToRespawn()
+        {
+            yield return new WaitForSeconds(_respawnTime);
+            Respawn();
+        }
+
+        private void Explode()
+        {
+            isDead = true;
+            visuals.SetActive(false);
+            // play particle systems
+            // play sound
+            StartCoroutine(CorWaitToRespawn());
+        }
+
+        private void Prepare()
+        {
+            // Load configurable values from Scriptable Object
+            _maxHealth = carParametersSo.MaxHealth;
+            _respawnTime = carParametersSo.RespawnTime;
+            _respawnPosition = transform.position;
+            _damageCooldown = carParametersSo.DamageCooldown;
+            _minDamage = carParametersSo.MinDamage;
+            _maxDamage = carParametersSo.MaxDamage;
+            
+            
+            _rb = GetComponent<Rigidbody>();
+            
+            _carImpulse = GetComponent<NetworkCarImpulse>();
+            // Subscribe to punch event
+            _carImpulse.DoPunchEvent += Punched;
+            _healthIndicatorRenderer = healthIndicator.GetComponent<MeshRenderer>();
+        }
+}
--- Diooo/Assets/_LazyGamesMx/_Code/Goal/MultiplayerGoal.cs
+++ Diooo/Assets/_LazyGamesMx/_Code/Goal/MultiplayerGoal.cs
@@ -5,78 +5,47 @@ using UnityEngine;
 
 namespace com.LazyGames.Dio
 {
-    public class MultiplayerGoal :  NetworkBehaviour
+    public class MultiplayerGoal :  MonoBehaviour
     {
         
+        // public override void OnNetworkSpawn()
+        // {
+        //     DioGameManagerMultiplayer.Instance.OnGameStateChange += HandleGameStateChange;
+        // }
 
-        public override void OnNetworkSpawn()
-        {
-            DioGameManagerMultiplayer.Instance.OnGameStateChange += HandleGameStateChange;
-        }
-
-        private void HandleGameStateChange(DioGameManagerMultiplayer.GameStates state)
-        {
-            if (state == DioGameManagerMultiplayer.GameStates.GamePlaying)
-            {
-
-            }
-        }
+        // private void HandleGameStateChange(DioGameManagerMultiplayer.GameStates state)
+        // {
+        //     if (state == DioGameManagerMultiplayer.GameStates.GamePlaying)
+        //     {
+        //
+        //     }
+        // }
         
         private void OnTriggerEnter(Collider other)
         {
             NetworkCarData networkCar = other.GetComponent<NetworkCarData>();
-            
-            if (networkCar != null)
-            {
-                Debug.Log("El auto " + networkCar.OwnerClientId + " ha completado todas las vueltas requeridas");
-                DioGameManagerMultiplayer.Instance.OnPlayerFinishRace?.Invoke(networkCar.OwnerClientId);
+            if (networkCar != null) 
+            { 
+                Debug.Log("El auto = " + networkCar.OwnerClientId + " ha cruzado la meta"); 
+                DioGameManagerMultiplayer.Instance.OnPlayerCompletedRace?.Invoke(true);
             }
             
+            // Debug.Log("Auto pasa triguer");
             
-            // Debug.Log("<color=#DFFE97>On trigger + = </color>" +  auto.name);
+            // Vector3 direccionDeColision = other.ClosestPoint(transform.position) - transform.position;
+            // Vector3 direccionFrontal = transform.forward;
             //
+            // bool esCaraFrontal = Vector3.Dot(direccionDeColision.normalized, direccionFrontal) > 0.5f;
             //
-            // if (auto != null && direccionCorrectaPorAuto.ContainsKey(auto))
+            // if (esCaraFrontal)
             // {
-            //     Vector3 direccionDeColision = other.ClosestPoint(transform.position) - transform.position;
-            //     Vector3 direccionFrontal = transform.forward;
-            //
-            //     bool esCaraFrontal = Vector3.Dot(direccionDeColision.normalized, direccionFrontal) > 0.5f;
-            //
-            //     if (esCaraFrontal)
-            //     {
-            //         if (direccionCorrectaPorAuto[auto])
-            //         {
-            //             vueltasPorAuto[auto]++;
-            //         }
-            //         else
-            //         {
-            //             direccionCorrectaPorAuto[auto] = true;
-            //         }
-            //     }
-            //     else
-            //     {
-            //         direccionCorrectaPorAuto[auto] = false;
-            //     }
-            //
-            //     if (vueltasPorAuto[auto] == vueltasNecesarias)
-            //     {
-            //         Debug.Log("El auto " + auto.name + " ha completado todas las vueltas requeridas");
-            //         autosCompletados++;
-            //
-            //         if (autosCompletados == vueltasPorAuto.Count)
-            //         {
-            //             if(IsServer)
-            //                 OnAllAutosCompleted();
-            //         }
-            //     }
-            // }
+            
         }
 
-        private void OnAllAutosCompleted()
-        {
-            DioGameManagerMultiplayer.Instance.OnAllPlayersCompleteRace?.Invoke();
-        }
+        // private void OnAllAutosCompleted()
+        // {
+        //     DioGameManagerMultiplayer.Instance.OnAllPlayersCompleteRace?.Invoke();
+        // }
         
     }
 }
