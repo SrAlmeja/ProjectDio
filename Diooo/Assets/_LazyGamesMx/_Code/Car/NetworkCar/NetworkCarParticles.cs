d62fa668f359f408d268da32aac76e5c28065b3a
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2023-06-06 23:59:15 -0600

    CARS MULTIPLAYER FUNCIONALES

---
+++ Diooo/Assets/_LazyGamesMx/_Code/Car/NetworkCar/NetworkCarParticles.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkCarParticles : NetworkBehaviour
+{
+        [Header("Objects with CarParticle component")]   
+        [SerializeField] private ParticleSystem sparksParticle;
+
+        [Header("Explosion Particles Systems")]
+        [SerializeField] private ParticleSystem explosionSmokeParticle;
+        [SerializeField] private ParticleSystem explosionFireParticle;
+        [SerializeField] private ParticleSystem explosionExplosionParticle;
+        [SerializeField] private ParticleSystem fireParticle;
+        
+        private NetworkCar_Respawn _carRespawn;
+
+        public override void OnNetworkSpawn()
+        {
+            Prepare();
+        }
+        
+        public void PlaySparksParticle(Vector3 pos)
+        {
+            sparksParticle.transform.position = pos;
+            sparksParticle.Play();
+        }
+        
+        private void PlayExplosionParticles()
+        {
+            explosionSmokeParticle.Play();
+            explosionFireParticle.Play();
+            explosionExplosionParticle.Play();
+            fireParticle.Play();
+        }
+        
+        private void StopExplosionParticles()
+        {
+            explosionSmokeParticle.Stop();
+            explosionFireParticle.Stop();
+            explosionExplosionParticle.Stop();
+            fireParticle.Stop();
+            
+            explosionFireParticle.Clear();
+            explosionExplosionParticle.Clear();
+            explosionSmokeParticle.Clear();
+            fireParticle.Clear();
+        }
+
+        private void Prepare()
+        {
+            _carRespawn = GetComponent<NetworkCar_Respawn>();
+            _carRespawn.OnDie += PlayExplosionParticles;
+            _carRespawn.OnRespawn += StopExplosionParticles;
+        }
+    }
